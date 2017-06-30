18febd7fb91f3cb1cc8a2a5997435b00bc7cfb49
Dayang Shen <dayshen@microsoft.com>
2017-06-30 13:58:53 +0800

    Fix Git command running on slave

---
+++ src/test/java/org/jenkinsci/plugins/microsoft/appservice/test/FilePathUtilsTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package org.jenkinsci.plugins.microsoft.appservice.test;
+
+import hudson.FilePath;
+import org.apache.commons.io.FilenameUtils;
+import org.jenkinsci.plugins.microsoft.appservice.util.FilePathUtils;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class FilePathUtilsTest {
+
+    @Rule
+    public TemporaryFolder workspaceDir = new TemporaryFolder();
+
+    @Test
+    public void trimDirectoryPrefix() throws Exception {
+        assertTrimDirectoryPrefix("a", "a/b/c.txt", "b/c.txt");
+        assertTrimDirectoryPrefix("a", "a/b.txt", "b.txt");
+        assertTrimDirectoryPrefix("", "c.txt", "c.txt");
+    }
+
+    private void assertTrimDirectoryPrefix(String sourceDirPath, String filePath, String expectedRemoteName) throws Exception {
+        final FilePath workspace = new FilePath(workspaceDir.getRoot());
+        final FilePath sourceDir = workspace.child(sourceDirPath);
+        final FilePath file = workspace.child(filePath);
+
+        final String remoteName = FilePathUtils.trimDirectoryPrefix(sourceDir, file);
+        Assert.assertEquals(expectedRemoteName, FilenameUtils.separatorsToUnix(remoteName));
+    }
+}
