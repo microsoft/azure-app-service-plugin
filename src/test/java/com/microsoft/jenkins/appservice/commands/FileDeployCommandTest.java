9f769a633d8e36f9c8fdbe2dd277c366d8c89f12
Jie Shen <drfish.me@gmail.com>
2018-08-29 15:17:53 +0800

    Add retry for zip and war deploy

--- src/test/java/com/microsoft/jenkins/appservice/commands/FileDeployCommandTest.java
+++ src/test/java/com/microsoft/jenkins/appservice/commands/FileDeployCommandTest.java
@@ -17,16 +17,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.TimeoutException;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class FileDeployCommandTest {
 
@@ -90,6 +89,54 @@ public class FileDeployCommandTest {
         verify(slot).warDeploy(any(InputStream.class), eq("ROOT"));
         verify(slot).warDeploy(any(InputStream.class), eq("app"));
         verify(app, never()).warDeploy(any(InputStream.class), anyString());
+    }
+
+    @Test
+    public void warDeploy_exception() throws IOException {
+        temporaryFolder.newFile("ROOT.war");
+        Run run = mock(Run.class);
+        FilePath workspace = new FilePath(temporaryFolder.getRoot());
+        Launcher launcher = mock(Launcher.class);
+        TaskListener listener = mock(TaskListener.class);
+        JobContext jobContext = new JobContext(run, workspace, launcher, listener);
+
+        FileDeployCommand.IFileDeployCommandData context = mock(FileDeployCommand.IFileDeployCommandData.class);
+        when(context.getJobContext()).thenReturn(jobContext);
+        when(context.getSourceDirectory()).thenReturn("");
+        WebApp app = mock(WebApp.class);
+        doThrow(IOException.class).when(app).warDeploy(any(InputStream.class), any(String.class));
+        when(context.getWebApp()).thenReturn(app);
+        when(context.getFilePath()).thenReturn("*.war");
+
+        FileDeployCommand command = new FileDeployCommand();
+        command.execute(context);
+
+        verify(app, times(3)).warDeploy(any(InputStream.class), any(String.class));
+        verify(context).logError(any(String.class));
+    }
+
+    @Test
+    public void warDeploy_exception_once() throws IOException {
+        temporaryFolder.newFile("ROOT.war");
+        Run run = mock(Run.class);
+        FilePath workspace = new FilePath(temporaryFolder.getRoot());
+        Launcher launcher = mock(Launcher.class);
+        TaskListener listener = mock(TaskListener.class);
+        JobContext jobContext = new JobContext(run, workspace, launcher, listener);
+
+        FileDeployCommand.IFileDeployCommandData context = mock(FileDeployCommand.IFileDeployCommandData.class);
+        when(context.getJobContext()).thenReturn(jobContext);
+        when(context.getSourceDirectory()).thenReturn("");
+        WebApp app = mock(WebApp.class);
+        doThrow(IOException.class).doNothing().when(app).warDeploy(any(InputStream.class), any(String.class));
+        when(context.getWebApp()).thenReturn(app);
+        when(context.getFilePath()).thenReturn("*.war");
+
+        FileDeployCommand command = new FileDeployCommand();
+        command.execute(context);
+
+        verify(app, times(2)).warDeploy(any(InputStream.class), any(String.class));
+        verify(context, never()).logError(any(String.class));
     }
 
     @Test
@@ -170,4 +217,53 @@ public class FileDeployCommandTest {
         verify(app, never()).zipDeploy(any(InputStream.class));
     }
 
+    @Test
+    public void zipDeploy_exception() throws IOException {
+        temporaryFolder.newFile("ROOT.zip");
+
+        Run run = mock(Run.class);
+        FilePath workspace = new FilePath(temporaryFolder.getRoot());
+        Launcher launcher = mock(Launcher.class);
+        TaskListener listener = mock(TaskListener.class);
+        JobContext jobContext = new JobContext(run, workspace, launcher, listener);
+
+        FileDeployCommand.IFileDeployCommandData context = mock(FileDeployCommand.IFileDeployCommandData.class);
+        when(context.getJobContext()).thenReturn(jobContext);
+        when(context.getSourceDirectory()).thenReturn("");
+        WebApp app = mock(WebApp.class);
+        when(context.getWebApp()).thenReturn(app);
+        when(context.getFilePath()).thenReturn("*.zip");
+        doThrow(IOException.class).when(app).zipDeploy(any(InputStream.class));
+
+        FileDeployCommand command = new FileDeployCommand();
+        command.execute(context);
+
+        verify(app, times(3)).zipDeploy(any(InputStream.class));
+        verify(context).logError(any(String.class));
+    }
+
+    @Test
+    public void zipDeploy_exception_once() throws IOException {
+        temporaryFolder.newFile("ROOT.zip");
+
+        Run run = mock(Run.class);
+        FilePath workspace = new FilePath(temporaryFolder.getRoot());
+        Launcher launcher = mock(Launcher.class);
+        TaskListener listener = mock(TaskListener.class);
+        JobContext jobContext = new JobContext(run, workspace, launcher, listener);
+
+        FileDeployCommand.IFileDeployCommandData context = mock(FileDeployCommand.IFileDeployCommandData.class);
+        when(context.getJobContext()).thenReturn(jobContext);
+        when(context.getSourceDirectory()).thenReturn("");
+        WebApp app = mock(WebApp.class);
+        when(context.getWebApp()).thenReturn(app);
+        when(context.getFilePath()).thenReturn("*.zip");
+        doThrow(IOException.class).doNothing().when(app).zipDeploy(any(InputStream.class));
+
+        FileDeployCommand command = new FileDeployCommand();
+        command.execute(context);
+
+        verify(app, times(2)).zipDeploy(any(InputStream.class));
+        verify(context, never()).logError(any(String.class));
+    }
 }
